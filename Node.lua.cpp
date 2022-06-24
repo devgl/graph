a317181bbc5832d7a667302feab95b0ce6a02329
devgl <742763031@qq.com>
2022-06-24 21:38:27 +0800

    refactroing.

--- Node.lua.cpp
+++ Node.lua.cpp
@@ -2,14 +2,23 @@
 
 int Lua_CreateNode(lua_State* l)
 {
-
-
+	size_t len = 0;
+	const char* name = lua_tolstring(l, 1, &len);
+	std::string strName(name, len);
+	Node* pNode = new Node(strName);
+	lua_pushlightuserdata(l, pNode);
 	return 1;
 }
 
 int Lua_AddInput(lua_State* l)
 { 
+	Node* pNode = (Node*)lua_touserdata(l, 1);
 
+	size_t len = 0;
+	const char* name = lua_tolstring(l, 2, &len);
+	std::string strName(name, len);
+
+	pNode->AddInput(strName);
 
 	return 1;
 }
@@ -20,6 +29,7 @@ int Node::LuaRegister(lua_State* l)
 	{
 		luaL_Reg{"Create", Lua_CreateNode},
 		luaL_Reg{"AddInput", Lua_AddInput},
+		luaL_Reg{nullptr, nullptr},
 	};
 	luaL_newlib(l, apis);
 	return 1;
--- scripts/main.lua
+++ scripts/main.lua
@@ -1,4 +1,24 @@
 local tb = open_GraphLib()
 
-print(tb)
-tb.Link();
+local NodeTable = open_NodeLib()
+NodeTable.mt = 
+{ 
+	__index = function(tb, key) 
+		return function(t, ...) 
+			print(...)
+			NodeTable[key](t._pNode, ...)
+		end
+	end
+}
+
+function NodeTable.new(name)
+	local tb = {_pNode = NodeTable.Create(name)}
+	setmetatable(tb, NodeTable.mt)
+	return tb
+end
+
+local dummyNode = NodeTable.new("dummy")
+dummyNode:AddInput("hehe")
+
+
+
